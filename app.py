dda652315a26179585baf9abf35d9fa0a6ca3597
Jon Mentzell <jmentzell@redhat.com>
2021-03-27 14:09:27 -0400

    templates / css addition

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
+import configparser
+
+# Get the configuration from the ini file.
+config = configparser.ConfigParser()
+config.sections
+config.read('app.ini')
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
 @app.route('/md')
 def mdvax():
-    return 'INDEX'
+    return render_template('state.html')
 
 if __name__ == '__main__':
     app.run()
